8fdeef9404c28e8ae69dfc39a8f4e173fa738d9f
matheus melo <matheus.barreto@eaportal.org>
2024-06-09 18:58:39 +0000

    feat: instalação e implantação do react-router-dom e do styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-export default function App() {
-  return (
-    <div>App</div>
-  )
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+import { ThemeProvider } from "styled-components"
+
+import { Home } from "./pages/Home"
+import { theme } from "./styles/theme.js"
+
+const router = createBrowserRouter ([
+  {
+path: "/",
+element: < Home />
 }
+])
+export function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <RouterProvider router ={router}/>
+    </ThemeProvider>
+  )
+} 
---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+    homeBackground: "#131518", 
+    cardGradient: "radial-gradient(98.96% 98.96% at 50% 0%, #232A34 0%, #181e27 100%)",
+    textColor: "#969FAD",
+    submitButtonColor: "#Fc7614",
+    white: "#fff",
+    feedbackButtonColor: "#262E38",
+    feedbackButtonText: "#7c8798"
+}
