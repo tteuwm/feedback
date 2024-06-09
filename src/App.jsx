24f3c061426a38a192a6e2612df7937835af6a79
matheus melo <matheus.barreto@eaportal.org>
2024-06-09 19:12:58 +0000

    style:criação dos estilos globais

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "styled-components"
 
 import { Home } from "./pages/Home"
 import { theme } from "./styles/theme.js"
+import { GlobalStyle } from "./styles/global.js"
 
 const router = createBrowserRouter ([
   {
@@ -15,6 +16,7 @@ export function App() {
   return (
     <ThemeProvider theme={theme}>
       <RouterProvider router ={router}/>
+      <GlobalStyle />
     </ThemeProvider>
   )
 } 
